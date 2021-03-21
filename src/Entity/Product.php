6ebd4131d9779a31e80555d3beea550970b76745
Josso Cedric <cjosso@gmail.com>
2021-03-21 16:45:58 +0100

    UPDATE: Entity Product -> relation with Category

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -57,6 +57,12 @@ class Product
      */
     private $updatedAt;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -154,6 +160,18 @@ class Product
     public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
