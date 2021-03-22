311b3d10e0815415efa0f68edbcadb3329a4714c
github_pierreFerre <github_pedr1ferre@gmail.com>
2021-03-22 12:33:59 +0100

    CREATE: route read one category

--- src/Controller/Api/CategoryController.php
+++ src/Controller/Api/CategoryController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Category;
+use App\Repository\ProductRepository;
 use App\Repository\CategoryRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategoryController extends AbstractController
 {
     /**
-     * Read all the categories
+     * Browse all the categories
      * 
      * @Route("/api/category", name="api_category", methods={"GET"})
      */
@@ -19,5 +21,27 @@ class CategoryController extends AbstractController
         $categories = $categoryRepository->findAll();
 
         return $this->json($categories, 200);
+    }
+
+    /**
+     * Read one category
+     * 
+     * @Route("/api/category/{id<\d+>}", name="api_category_read_one", methods={"GET"})
+     */
+    public function read(Category $category = null, ProductRepository $productRepository): Response
+    {
+        // We send a custom message if category not found (404)
+        if ($category === null) {
+            $message = [
+                'status' => Response::HTTP_NOT_FOUND,
+                'error' => 'Catégorie non trouvée.',
+            ];
+
+            return $this->json($message, Response::HTTP_NOT_FOUND);
+        }
+
+        $products = $productRepository->findBy(['category' => $category]);
+
+        return $this->json($products, 200);
     }
 }
