f2e84eaaa7e89018c423e9bb020ea5863b966928
github_pierreFerre <github_pedr1ferre@gmail.com>
2021-03-22 14:06:06 +0100

    CREATE: route browse delivery points

---
+++ src/Controller/Api/ProductController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProductController extends AbstractController
+{
+    /**
+     * Read one product
+     * 
+     * @Route("/api/product/{id<\d+>}", name="api_read_one_product", methods={"GET"})
+     */
+    public function read(Product $product): Response
+    {
+        // We send a custom message if product not found (404)
+        if ($product === null) {
+
+            // Optionnel, message pour le front
+            $message = [
+                'status' => Response::HTTP_NOT_FOUND,
+                'error' => 'Film non trouvé.',
+            ];
+
+            // On défini un message custom et un status code HTTP 404
+            return $this->json($message, Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->json($product, 200);
+    }
+}
