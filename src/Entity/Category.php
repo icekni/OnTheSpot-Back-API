8da82db0e57a2539a3f71b608623498a4a3f90f8
Josso Cedric <cjosso@gmail.com>
2021-03-23 16:25:00 +0100

    UPDATE: Constraints on Category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -4,7 +4,6 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\CategoryRepository;
-use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Validator\Constraints AS Assert;
 
 /**
