5d00febf69b9ea0e1666daced74dc12559861dc2
Josso Cedric <cjosso@gmail.com>
2021-03-25 11:20:32 +0100

    UPDATE: mailer try/catch

--- src/Controller/Back/MainController.php
+++ src/Controller/Back/MainController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 
 class MainController extends AbstractController
 {
@@ -36,7 +37,11 @@ class MainController extends AbstractController
             ->text('Sending emails is fun again!')
             ->html('<p>See Twig integration for better HTML integration!</p>');
 
-        $mailer->send($email);
+        try {
+            $mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            dump($e);
+        }
 
         return $this->redirectToRoute('back_main');
     }
