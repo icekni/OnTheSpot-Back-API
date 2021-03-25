222f8b00e28c0b44737a3e2f5be6837c77c90887
Josso Cedric <cjosso@gmail.com>
2021-03-25 11:13:39 +0100

    CREATE: test sending mail

--- src/Controller/Back/MainController.php
+++ src/Controller/Back/MainController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller\Back;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
@@ -16,5 +18,26 @@ class MainController extends AbstractController
         return $this->render('back/main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
+    }
+
+    /**
+     * @Route("/test", name="back_test")
+     */
+    public function mail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('onthespot@apotheoz.tech')
+            ->to('cjosso@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+        return $this->redirectToRoute('back_main');
     }
 }
