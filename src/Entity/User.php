eecf29f89afb76594ea8411ae0a6d45f4482509c
github_pierreFerre <github_pedr1ferre@gmail.com>
2021-03-25 10:44:09 +0100

    UPDATE: use Groups in user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints AS Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
