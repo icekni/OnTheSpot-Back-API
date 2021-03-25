06caa3d84e3366006a614a1728a49986630f85c4
Josso Cedric <cjosso@gmail.com>
2021-03-25 12:07:46 +0100

    UPDATE: API -> add user with confirmation email

--- src/Controller/Back/MainController.php
+++ src/Controller/Back/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Back;
 
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,27 +22,29 @@ class MainController extends AbstractController
         ]);
     }
 
+    public function comfirmMail($token): Response
+    {
+        return $this->render('back/confirm.html.twig');
+    }
+
     /**
      * @Route("/test", name="back_test")
      */
     public function mail(MailerInterface $mailer): Response
     {
-        $email = (new Email())
-            ->from('onthespot@apotheoz.tech')
-            ->to('cjosso@gmail.com')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject('Time for Symfony Mailer!')
-            ->text('Sending emails is fun again!')
-            ->html('<p>See Twig integration for better HTML integration!</p>');
+        dd($random = bin2hex(random_bytes(10)));
+        // $email = (new Email())
+        //     ->from(new Address('onthespot@apotheoz.tech', 'OnTheSpot'))
+        //     ->to('cjosso@gmail.com')
+        //     ->subject('Time for Symfony Mailer!')
+        //     ->text('Sending emails is fun again!')
+        //     ->html('<p>See Twig integration for better HTML integration!</p>');
 
-        try {
-            $mailer->send($email);
-        } catch (TransportExceptionInterface $e) {
-            dump($e);
-        }
+        // try {
+        //     $mailer->send($email);
+        // } catch (TransportExceptionInterface $e) {
+        //     // Nothing
+        // }
 
         return $this->redirectToRoute('back_main');
     }
