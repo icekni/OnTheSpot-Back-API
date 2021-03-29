96b8a94e81f0c68fb5b3216116e7aba84ba61046
Josso Cedric <cjosso@gmail.com>
2021-03-29 22:42:21 +0200

    UPDATE: requirement on route product_show -> \d+ (id must be a int)

--- src/Controller/Back/ProductController.php
+++ src/Controller/Back/ProductController.php
@@ -57,7 +57,7 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="product_show", methods={"GET"})
+     * @Route("/{id<\d+>}", name="product_show", methods={"GET"})
      */
     public function show(Product $product): Response
     {
