230938ed975fdb62be292c4df234e174cc368cfc
Josso Cedric <cjosso@gmail.com>
2021-03-29 14:25:15 +0200

    UPDATE: order controller -> send a mail after changing the status

--- src/Controller/Back/OrderController.php
+++ src/Controller/Back/OrderController.php
@@ -4,12 +4,16 @@ namespace App\Controller\Back;
 
 use App\Entity\Order;
 use App\Form\OrderType;
+use Symfony\Component\Mime\Email;
 use App\Repository\OrderRepository;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 
 /**
  * @Route("/order")
@@ -63,13 +67,38 @@ class OrderController extends AbstractController
     /**
      * @Route("/{id}/edit", name="order_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, Order $order): Response
+    public function edit(Request $request, Order $order, Mailer $mailer): Response
     {
+        $oldstatus = $order->getStatus();
+
         $form = $this->createForm(OrderType::class, $order);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
+
+            $user = $order->getUser();
+
+            $status = [
+                'En attente de préparation',
+                'En préparation',
+                'En livraison'
+            ];
+
+            // if the status is not the same as $oldStatus, it means status has been modified, we need to send a mail
+            if ($order->getStatus() !== $oldstatus && $order->getStatus() < 3) {
+                $email = (new Email())
+                    ->from(new Address('onthespot@apotheoz.tech', 'OnTheSpot'))
+                    ->to($user->getEmail())
+                    ->subject('Votre commande #' . $order->getId() . ' est maintenant ' . $status[$order->getStatus()])
+                    ->text('Bonjour ' . $user->getFirstname() . '\nNous avons le plaisir de vous annoncer que votre commande est maintenant ' . $order->getId() . '.')
+                    ->html('<p>Bonjour ' . $user->getFirstname() . '</p><p>Nous avons le plaisir de vous annoncer que votre commande est maintenant ' . $order->getId() . '.');
+
+                try {
+                    $mailer->send($email);
+                } catch (TransportExceptionInterface $e) {
+                }
+            }
 
             return $this->redirectToRoute('order_show', ['id' => $order->getId()]);
         }
@@ -85,7 +114,7 @@ class OrderController extends AbstractController
      */
     public function delete(Request $request, Order $order): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$order->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $order->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($order);
             $entityManager->flush();
