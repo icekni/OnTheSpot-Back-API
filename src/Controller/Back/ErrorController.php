5ebaf7553802913bf6a2bc701e439a41490d5a7a
github_pierreFerre <github_pedr1ferre@gmail.com>
2021-03-29 11:26:31 +0200

    CREATE: 404 page unfinished

---
+++ src/Controller/Back/ErrorController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Back;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ErrorController extends AbstractController
+{
+    /**
+     * @Route("/back/error", name="back_error")
+     */
+    public function index(): Response
+    {
+        return $this->render('back/error/index.html.twig', [
+            'controller_name' => 'ErrorController',
+        ]);
+    }
+}
