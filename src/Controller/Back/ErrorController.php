214b9b04ad2053ddf6ce3e24c212d8bede97779b
github_pierreFerre <github_pedr1ferre@gmail.com>
2021-03-31 10:55:52 +0200

    UPDATE: modification on error controller

--- src/Controller/Back/ErrorController.php
+++ src/Controller/Back/ErrorController.php
@@ -13,16 +13,6 @@ class ErrorController extends AbstractController
      */
     public function index(): Response
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
         return $this->render('back/error/index.html.twig');
-=======
-        return $this->render('back/error/index.html.twig', [
-            'controller_name' => 'ErrorController',
-        ]);
->>>>>>> 5ebaf75... CREATE: 404 page unfinished
-=======
-        return $this->render('back/error/index.html.twig');
->>>>>>> 3d11531... UPDATE: 404-403 pages done
     }
 }
