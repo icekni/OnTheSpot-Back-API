89c77ecd9b27b7d61300906939e11419f0f2ab9b
Josso Cedric <cjosso@gmail.com>
2021-04-05 18:52:09 +0200

    UPDATE: cors policy

--- src/EventListener/ResponseListener.php
+++ src/EventListener/ResponseListener.php
@@ -15,7 +15,7 @@ class ResponseListener
         if ($response->headers->get('Content-Type') === 'application/json') {
             // Allow access to the API from everywhere
             $response->headers->set('Access-Control-Allow-Origin', '*');
-            $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT, PATCH');
+            $response->headers->set('access-control-allow-methods', 'POST, GET, OPTIONS, DELETE, PUT, PATCH');
             $response->headers->set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         }
     }
